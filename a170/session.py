a3883c2f6a1b125f730ba97903ff93c1023e357a
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2018-10-30 17:24:19 +0800

    simplify imports in the session module

--- a170/session.py
+++ a170/session.py
@@ -1,17 +1,14 @@
 # coding: utf-8
-import urllib3
-from urllib3.util.retry import Retry
 from requests_html import AsyncHTMLSession, HTMLSession, requests
 
-urllib3.disable_warnings()
+requests.urllib3.disable_warnings()
 
 
 def retry_session(retries=5, session=HTMLSession()):
-    retry = Retry(
+    retry = requests.urllib3.util.retry.Retry(
         total=retries,
         read=retries,
         connect=retries,
-        backoff_factor=0,
         status_forcelist=(500, 502, 503, 504),
     )
     adapter = requests.adapters.HTTPAdapter(max_retries=retry)
